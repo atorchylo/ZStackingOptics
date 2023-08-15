4dcffcd2d3e484ab263c32b8835d9c857e8923dc
Andrii Torchylo <andrii.torchylo@gmail.com>
2023-08-14 22:50:29 -0700

    blur update

--- optics.py
+++ optics.py
@@ -1,5 +1,4 @@
 import numpy as np
-from plotters import plot_kernels
 from fourier import FFT2, IFFT2
 
 class FreeSpace:
@@ -7,31 +6,61 @@ class FreeSpace:
         self.L = L
         self.k_z = k_z
         self.coordinates = coordinates
+        self.kernel = self.get_kernel()
         
     def get_kernel(self):
         nu_x, nu_y = self.coordinates.get_frequency_axis()
         return np.exp(1j*(2*np.pi**2)*(nu_x**2 + nu_y**2) * self.L / self.k_z)
+    
+    def propagate(self, field):
+        fourrier_field = FFT2(field, self.coordinates.dx)
+        fourrier_field *= self.kernel
+        field = IFFT2(fourrier_field, self.coordinates.dx)
+        return field
     
 class Lens:
     def __init__(self, f, k_z, coordinates):
         self.f = f
         self.k_z = k_z
         self.coordinates = coordinates
+        self.kernel = self.get_kernel()
     
     def get_kernel(self):
         x, y = self.coordinates.get_position_axis()
         return np.exp(1j*self.k_z*(x**2 + y**2)/(2*self.f))
     
+    def propagate(self, field):
+        return field * self.kernel
+    
 class CircularAperture:
     def __init__(self, D, coordinates):
         self.D = D
         self.coordinates = coordinates
+        self.kernel = self.get_kernel()
     
     def get_kernel(self):
         x, y = self.coordinates.get_position_axis()
         if self.D is None:
             return (x**2 + y**2 > -1).astype(float)
         return (x**2 + y**2 <= self.D**2/4).astype(float)
+    
+    def propagate(self, field):
+        return field * self.kernel
+    
+class GaussianAperture:
+    def __init__(self, D, coordinates):
+        self.D = D
+        self.coordinates = coordinates
+        self.kernel = self.get_kernel()
+    
+    def get_kernel(self):
+        x, y = self.coordinates.get_position_axis()
+        if self.D is None:
+            return (x**2 + y**2 > -1).astype(float)
+        return np.exp(-(x**2 + y**2)/(self.D**2/4))
+    
+    def propagate(self, field):
+        return field * self.kernel
 
 class SpaceLensSpaceSystem:
     def __init__(self, a, b, f, D, k_z, coordinates):
@@ -42,35 +71,67 @@ class SpaceLensSpaceSystem:
         self.coordinates = coordinates 
         
         # get propagation kernels
-        self.space1 = FreeSpace(a, k_z, coordinates).get_kernel()
-        self.lens = Lens(f, k_z, coordinates).get_kernel()
-        self.aperture = CircularAperture(D, coordinates).get_kernel()
-        self.space2 = FreeSpace(b, k_z, coordinates).get_kernel()
+        self.space_a = FreeSpace(a, k_z, coordinates)
+        self.lens = Lens(f, k_z, coordinates)
+        self.aperture = CircularAperture(D, coordinates)
+        self.space_b = FreeSpace(b, k_z, coordinates)
         
-    def propagate(self, u_field):
+    def propagate(self, field):
         # propagate distance a
-        fourrier_u_field = FFT2(u_field, self.coordinates.dx)
-        fourrier_u_field *= self.space1
-        u_field = IFFT2(fourrier_u_field, self.coordinates.dx)
+        field = self.space_a.propagate(field)
         
         # propagate through the lens
-        u_field *= self.lens
+        field = self.lens.propagate(field)
         
         # propagate through the aperture
-        u_field *= self.aperture
+        field = self.aperture.propagate(field)
         
         # propagate distance b
-        fourrier_u_field = FFT2(u_field, self.coordinates.dx)
-        fourrier_u_field *= self.space2
-        u_field = IFFT2(fourrier_u_field, self.coordinates.dx)
-        return u_field
+        field = self.space_b.propagate(field)
+        return field
+
+class GaussianPSF:
+    def __init__(self, delta, k, b, D, coordinates):
+        self.delta = delta
+        self.D = D
+        self.k = k
+        self.w0 = 4 * b / (D * k)
+        self.zR = 8 * b**2 / (D**2 * k)
+        self.w = self.w0 * np.sqrt(1 + (b**2 * delta / self.zR)**2)
+        self.coordinates = coordinates
+        self.kernel = self.get_kernel()
     
-    def plot_kernels(self, zoom=None):
-        plot_kernels(
-            self.space1, 
-            self.lens, 
-            self.aperture, 
-            self.space2, 
-            self.coordinates, 
-            zoom
-        )
+    def get_kernel(self):
+        x, y = self.coordinates.get_position_axis()
+        kernel = self.w0 / self.w * np.exp(-(x**2 + y**2) / self.w**2)
+        return kernel
+    
+    def propagate(self, field):
+        fourrier_field = FFT2(field, self.coordinates.dx)
+        fourrier_field *= self.kernel
+        field = IFFT2(fourrier_field, self.coordinates.dx)
+        return field
+    
+    
+class GaussianIntensityPSF:
+    def __init__(self, delta, k, b, D, coordinates):
+        self.delta = delta
+        self.D = D
+        self.k = k
+        self.w0 = 4 * b / (D * k)
+        self.zR = 8 * b**2 / (D**2 * k)
+        self.w = self.w0 * np.sqrt(1 + (b**2 * delta / self.zR)**2)
+        self.coordinates = coordinates
+        self.kernel = self.get_kernel()
+        
+    def get_kernel(self):
+        x, y = self.coordinates.get_position_axis()
+        kernel = (self.w0 / self.w)**2 * np.exp(- 2 * (x**2 + y**2) / self.w**2)
+        return kernel
+    
+    def propagate(self, img):
+        fourrier_img = FFT2(img, self.coordinates.dx)
+        fourrier_kernel = FFT2(self.kernel, self.coordinates.dx)
+        fourrier_img *= fourrier_kernel
+        img = IFFT2(fourrier_img, self.coordinates.dx)
+        return np.abs(img)
--- plotters.py
+++ plotters.py
@@ -1,14 +1,14 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-def plot_2d(field, coord, fourier_space=False, title=None, zoom=None):
+def plot_field(field, coord, fourier_space=False, title=None, zoom=None):
     """Plots absolute value and phase  of the field"""
     fig, ax = plt.subplots(1, 4, figsize=(14, 7), gridspec_kw={'width_ratios': [12, 1, 12, 1]})
     extent = coord.freq_extent if fourier_space else coord.pos_extent
     abs_img = ax[0].imshow(np.abs(field), extent=extent)
     plt.colorbar(abs_img, ax[1])
     phase_img = ax[2].imshow(np.angle(field), extent=extent)
-    plt.colorbar(abs_img, ax[3])
+    plt.colorbar(phase_img, ax[3])
     if title is not None:
         ax[0].set_title('abs of ' + title)
         ax[2].set_title('phase of ' + title)
@@ -21,20 +21,21 @@ def plot_2d(field, coord, fourier_space=False, title=None, zoom=None):
     ax[2].set_yticks([])
     plt.show()
     
-def plot_kernels(space1, lens, aperture, space2, coord, zoom):
-    fig, ax = plt.subplots(1, 4, figsize=(20, 5))
-    ax[0].imshow(space1.real, extent=coord.freq_extent)
-    ax[0].set_title(f'propagation by L=a kernel (zoom={zoom})')
-    ax[1].imshow(lens.real, extent=coord.pos_extent)
-    ax[1].set_title('lens kernel')
-    ax[2].imshow(aperture, extent=coord.pos_extent)
-    ax[2].set_title('aperture kernel')
-    ax[3].imshow(space2.real, extent=coord.freq_extent)
-    ax[3].set_title(f'propagation by L=b kernel (zoom={zoom})')
+def plot_img(img, coord, zoom=None, FT=False, title=None):
+    """Plot's a 2d scalar array along with 1d plot of middle row pixels"""
+    extent = coord.freq_extent if FT else coord.pos_extent
+    ticks = coord.nu if FT else coord.x
+    
+    # plot
+    fig, ax = plt.subplots(1, 2, figsize=(12, 6))
+    ax[0].imshow(img, extent = extent, cmap='coolwarm')
+    ax[1].plot(ticks, img[img.shape[0]//2, :])
+    
     if zoom is not None:
         ax[0].set_xlim(-zoom, zoom)
         ax[0].set_ylim(-zoom, zoom)
-        ax[3].set_xlim(-zoom, zoom)
-        ax[3].set_ylim(-zoom, zoom)
-    plt.tight_layout()
+        ax[1].set_xlim(-zoom, zoom)
+        
+    if title is not None:
+        fig.suptitle(title)
     plt.show()
